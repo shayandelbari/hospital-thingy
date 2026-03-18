cbf4784f480a6fa09b4f1873fbee1e2ee25bac27
shayan <shayan.delbari@protonmail.com>
2026-03-18 17:08:16 -0400

    init

---
+++ src/main/java/com/hospital_thingy/HospitalThingyApplication.java
@@ -0,0 +1,13 @@
+package com.hospital_thingy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalThingyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalThingyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital_thingy/HospitalThingyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital_thingy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalThingyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
