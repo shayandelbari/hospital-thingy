63246c26c5ebbc7c343a723af8ac7fa67d0a8c5f
shayan <shayan.delbari@protonmail.com>
2026-03-18 21:25:08 -0400

    small fixed to the implementation of medical record

--- src/main/java/com/hospital_thingy/entity/VitalSign.java
+++ src/main/java/com/hospital_thingy/entity/VitalSign.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class VitalSign extends MedicalRecord {
     private int weight;
     private int heartRate;
-    private int bloodPressure;
+    private int[] bloodPressure;
     private int temperature;
     private int o2Stats;
 }
