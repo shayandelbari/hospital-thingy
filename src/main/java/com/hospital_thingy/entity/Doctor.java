c18187112eb53003e536d88d4f04700a63bc2203
jorge <jorgeh_92@hotmail.com>
2026-03-19 20:00:21 -0400

    test commit

--- src/main/java/com/hospital_thingy/entity/Doctor.java
+++ src/main/java/com/hospital_thingy/entity/Doctor.java
@@ -1,4 +1,28 @@
 package com.hospital_thingy.entity;
 
+import java.util.List;
+
 public class Doctor {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String licenseNumber;
+    private Speciality[] specialities;
+    private List<Appointment> appointments;
+
+    public enum Speciality
+    {
+        CARDIOLOGY,
+        DERMATOLOGY,
+        NEUROLOGY,
+        PEDIATRICS,
+        ORTHOPEDICS,
+        GYNECOLOGY,
+        ONCOLOGY,
+        PSYCHIATRY,
+        OPHTHALMOLOGY,
+        GASTROENTEROLOGY
+    }
 }
+
+
