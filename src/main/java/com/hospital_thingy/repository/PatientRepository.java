9836b333a8b011249dd698296ab41e21fa341a40
Brett Trudel <trudel25@gmail.com>
2026-04-01 17:15:57 -0400

    Suggestion in PatientRepository

--- src/main/java/com/hospital_thingy/repository/PatientRepository.java
+++ src/main/java/com/hospital_thingy/repository/PatientRepository.java
@@ -2,3 +2,7 @@ package com.hospital_thingy.repository;
 
 public class PatientRepository {
 }
+
+
+//Suggested TODO - add a custom method to get all MedicalRecords for a specific Patient in this repository
+    // lmk if you want me (Brett) to take care of this
