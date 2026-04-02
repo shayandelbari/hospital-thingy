262c5f9e190a27c07786e5c967da369cec431b78
jorge <jorgeh_92@hotmail.com>
2026-04-02 15:30:31 -0400

    DoctorServices(CRUD) missing exceptions

--- src/main/java/com/hospital_thingy/service/DoctorServices.java
+++ src/main/java/com/hospital_thingy/service/DoctorServices.java
@@ -1,11 +1,17 @@
 package com.hospital_thingy.service;
 
+import com.hospital_thingy.entity.Doctor;
 import com.hospital_thingy.mapper.DoctorMapper;
 import com.hospital_thingy.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoctorServices {
+
+    @Autowired
     private final DoctorRepository doctorRepository;
     private final DoctorMapper doctorMapper;
 
@@ -13,6 +19,30 @@ public class DoctorServices {
         this.doctorRepository = doctorRepository;
         this.doctorMapper = doctorMapper;
     }
+
+    public List<Doctor> getAllDoctors()
+    {
+        return doctorRepository.findAll();
+    }
+
+    public void AddDoctor(Doctor doctor)
+    {
+        doctorRepository.save(doctor);
+    }
+
+    public void UpdateDoctor(Doctor doctor)
+    {
+        doctorRepository.save(doctor);
+    }
+
+    public void DeleteDoctor(Doctor doctor)
+    {
+        doctorRepository.delete(doctor);
+    }
+
+    
+
+
 
     // Example: map incoming DTO to entity, save with repository, then map back to
     // DTO.
