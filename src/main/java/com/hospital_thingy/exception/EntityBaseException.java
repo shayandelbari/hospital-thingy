48b36bd795412d8e015d25eb83e5772ffb96bcf9
aleprado <alepradovi@gmail.com>
2026-04-02 15:27:02 -0400

    Changes exception extends from Exception to RuntimeException and complete Patientrepo

--- src/main/java/com/hospital_thingy/exception/EntityBaseException.java
+++ src/main/java/com/hospital_thingy/exception/EntityBaseException.java
@@ -1,6 +1,6 @@
 package com.hospital_thingy.exception;
 
-public class EntityBaseException extends Exception{
+public class EntityBaseException extends RuntimeException{
     public EntityBaseException(String m){
         super("Entity Error: " + (m.isBlank() ? m : "Error occurred while handling entity"));
     }
