24537454d02f2508c5dad637e077d6b861a8884a
shayan <shayan.delbari@protonmail.com>
2026-04-02 14:40:46 -0400

    fixed doctor repo

--- src/main/java/com/hospital_thingy/repository/DoctorRepository.java
+++ src/main/java/com/hospital_thingy/repository/DoctorRepository.java
@@ -1,7 +1,7 @@
 package com.hospital_thingy.repository;
 
 import com.hospital_thingy.entity.Doctor;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DoctorRepository extends CrudRepository<Doctor, Long> {
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 }
