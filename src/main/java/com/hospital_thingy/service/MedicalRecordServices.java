7e81dc96b4bd0ec1263915e2451463db10ab5c65
Brett Trudel <trudel25@gmail.com>
2026-04-10 13:30:24 -0400

    Debugging (not really, lol)

--- src/main/java/com/hospital_thingy/service/MedicalRecordServices.java
+++ src/main/java/com/hospital_thingy/service/MedicalRecordServices.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 // reference documentation: https://www.javaguides.net/2025/03/spring-boot-architecture.html
 @Service
